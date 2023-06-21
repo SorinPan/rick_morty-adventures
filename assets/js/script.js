7e3f9cf9c60332af3eda63729575cb209a677b64
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-06-21 09:57:56 +0200

    Create array of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,16 @@ const instructions = document.getElementById("instructions");
 const instructionBtn = document.getElementById("btn-instructions");
 const closeBtn = document.getElementById("btn-close");
 
+const cards = [
+    'assets/images/flipcard1.png',
+    'assets/images/flipcard3.png',
+    'assets/images/flipcard4.png',
+    'assets/images/flipcard5.png',
+    'assets/images/flipcard6.png',
+    'assets/images/flipcard7.png',
+    'assets/images/flipcard8.png',
+];
+
 // Event Listeners
 
 instructionBtn.addEventListener("click", getInstructions);
