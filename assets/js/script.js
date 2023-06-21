e570cfb7445d152ccf0ecc036bd6206b88434372
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-06-21 08:56:10 +0200

    Add functions to show and hide instructions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("Testing link!");
+// Global variables
+
+const intro = document.getElementById("intro");
+const instructions = document.getElementById("instructions");
+const instructionBtn = document.getElementById("btn-instructions");
+const closeBtn = document.getElementById("btn-close");
+
+// Event Listeners
+
+instructionBtn.addEventListener("click", getInstructions);
+closeBtn.addEventListener("click", hideInstructions);
+
+function getInstructions() {
+    instructions.style.display = "flex";
+    intro.style.display = "none";
+}
+
+function hideInstructions() {
+    instructions.style.display = "none";
+    intro.style.display = "flex";
+}
