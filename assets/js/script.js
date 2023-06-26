100ce56616bb1b28e73ee48f22f836d7e6bfc574
Sorin Panait <126383030+SorinPan@users.noreply.github.com>
2023-06-26 13:56:25 +0200

    Add function to generate the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,50 @@ const instructions = document.getElementById("instructions");
 const playBtn = document.querySelector(".btn-play");
 const instructionBtn = document.querySelector(".btn-instructions");
 const closeBtn = document.querySelector(".btn-close");
+const game = document.getElementById("game");
 
 const cards = [
-    'assets/images/flipcard1.png',
-    'assets/images/flipcard3.png',
-    'assets/images/flipcard4.png',
-    'assets/images/flipcard5.png',
-    'assets/images/flipcard6.png',
-    'assets/images/flipcard7.png',
-    'assets/images/flipcard8.png',
+    {
+        img: 'assets/images/flipcard1.png',
+        name: 'flipcard1',
+    },
+    {
+        img: 'assets/images/flipcard2.png',
+        name: 'flipcard2',
+    },
+    {
+        img: 'assets/images/flipcard3.png',
+        name: 'flipcard3',
+    },
+    {
+        img: 'assets/images/flipcard4.png',
+        name: 'flipcard4',
+    },
+    {
+        img: 'assets/images/flipcard5.png',
+        name: 'flipcard5',
+    },
+    {
+        img: 'assets/images/flipcard6.png',
+        name: 'flipcard6',
+    },
+    {
+        img: 'assets/images/flipcard7.png',
+        name: 'flipcard7',
+    },
+    {
+        img: 'assets/images/flipcard8.png',
+        name: 'flipcard8',
+    }
 ];
+
+const cardsList = [...cards, ...cards];
 
 // Event Listeners
 
 playBtn.addEventListener("click", function () {
     displaySection(gameBoard, true);
+    generateCards();
 });
 instructionBtn.addEventListener("click", function () {
     displaySection(instructions, true);
@@ -38,4 +67,17 @@ function displaySection(section, visible) {
     } else {
         intro.style.display = "flex";
     }
+};
+
+/**
+ * Generates cards
+ */
+function generateCards() {
+    cardsList.forEach((card) => {
+        const flipCard = document.createElement("img");
+        flipCard.setAttribute("src", card.img);
+        flipCard.setAttribute("data-name", card.name);
+        flipCard.classList.add("card");
+        game.appendChild(flipCard);
+    })
 };
